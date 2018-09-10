308de2754e2873b8fae3e79a3e2795da2859cc7e
Olaf Lüke <olaf@tinkerforge.com>
2018-09-10 17:16:30 +0200

    Complete partially auto-generated Python example

--- software/examples/python/example_read_ds18b20_temperature.py
+++ software/examples/python/example_read_ds18b20_temperature.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# FIXME: This example is incomplete
-
 HOST = "localhost"
 PORT = 4223
 UID = "XYZ" # Change XYZ to the UID of your One Wire Bricklet
@@ -29,6 +27,10 @@ if __name__ == "__main__":
         ow.write_command(0, 68) # CONVERT T (start temperature conversion)
         time.sleep(1) # Wait for conversion to finish
         ow.write_command(0, 190) # READ SCRATCHPAD
+
+        t_low = ow.read().data
+        t_high = ow.read().data
+        print('Temperature: {0} °C'.format((t_low | (t_high << 8))/16.0))
 
     raw_input("Press key to exit\n") # Use input() in Python 3
     ipcon.disconnect()
